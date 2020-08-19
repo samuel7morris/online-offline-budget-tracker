e0394318214eb435368c30ff286d29fc1643fab8
Sam Morris <63416066+samuel7morris@users.noreply.github.com>
2020-08-19 09:36:09 -0600

    heroku bug 3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -16,10 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"; 
 
 mongoose.connect(MONGODB_URI)
 
