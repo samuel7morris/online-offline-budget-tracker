fce8096cd0e162881f5163dae60d583652805d9e
Sam Morris <63416066+samuel7morris@users.noreply.github.com>
2020-08-19 09:26:28 -0600

    heroku bug two

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+var MONGODB_URI = process.env.MONGODB_URI || ("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 mongoose.connect(MONGODB_URI)
 
